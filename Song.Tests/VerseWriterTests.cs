773c569951b3e6c1d6d2479fbe3634dc43f191bc
shush2u <arnassiusa@gmail.com>
2024-11-07 21:31:31 +0200

    VerseWriterTests refactor.

--- Song.Tests/VerseWriterTests.cs
+++ Song.Tests/VerseWriterTests.cs
@@ -2,48 +2,27 @@ namespace Song.Tests
 {
     public class VerseWriterTests
     {
-        [Fact]
-        public void WriteVerse_SingleAnimalVerse()
+        [Theory]
+        [InlineData("fly")]
+        [InlineData("spider")]
+        [InlineData("horse")]
+
+        public void WriteVerse_SingleAnimalVerse_WorksAsExpected(string animal)
         {
             var writer = new VerseWriter();
             
             string expected =
-@"There was an old lady who swallowed a fly.
-I don't know why she swallowed a fly - perhaps she'll die!";
+@$"There was an old lady who swallowed a {animal}.
+I don't know why she swallowed a {animal} - perhaps she'll die!";
 
-            string actual = writer.WriteVerse("fly", "");
-
-            actual.Should().Be(expected);
-        }
-
-        [Fact]
-        public void WriteVerse_TwoAnimalVerse()
-        {
-            VerseWriter writer = new VerseWriter();
-
-            string expected =
-@"There was an old lady who swallowed a spider;
-That wriggled and wiggled and tickled inside her.
-She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - perhaps she'll die!";
-
-            List<(string, string)> animals = new List<(string, string)>([
-                ("fly", ""),
-                ("spider", "That wriggled and wiggled and tickled inside her.")
-                ]);
-
-            string actual = "";
-            foreach ((string, string) animal in animals)
-            {
-                actual = writer.WriteVerse(animal.Item1, animal.Item2);
-            }
+            string actual = writer.WriteVerse(animal, "");
 
             actual.Should().Be(expected);
         }
 
         [Theory]
         [MemberData(nameof(AnimalTestCases))]
-        public void WriteVerse_MultipleAnimals(List<(string, string)> input, string expected)
+        public void WriteVerse_MultipleAnimals_WorksAsExpected(List<(string, string)> input, string expected)
         {
             VerseWriter writer = new VerseWriter();
 
@@ -88,19 +67,41 @@ That wriggled and wiggled and tickled inside her.
 She swallowed the spider to catch the bird,
 She swallowed the bird to catch the fly;
 I don't know why she swallowed a fly - perhaps she'll die!"
+            },
+
+            new object[]
+            {
+                new List<(string, string)>(
+                [
+                    ("a", "ALine"),
+                    ("b", "BLine"),
+                    ("c", "CLine"),
+                    ("d", "DLine"),
+
+                ]),
+
+@"There was an old lady who swallowed a d;
+DLine
+She swallowed the d to catch the c,
+She swallowed the c to catch the b,
+She swallowed the b to catch the a;
+I don't know why she swallowed a a - perhaps she'll die!"
             }
         };
 
-        [Fact]
-        public void FinalVerse_WorksAsExpected()
+        [Theory]
+        [InlineData("horse")]
+        [InlineData("spider")]
+        [InlineData("fly")]
+        public void FinalVerse_WorksAsExpected(string animal)
         {
             string expected =
-@"There was an old lady who swallowed a horse...
+@$"There was an old lady who swallowed a {animal}...
 ...She's dead, of course!";
 
             var writer = new VerseWriter();
 
-            string actual = writer.FinalVerse("horse");
+            string actual = writer.FinalVerse(animal);
 
             actual.Should().Be(expected);
         }
